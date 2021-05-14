f4280b5fb3c85ae2191422e2a8048e2ca989df60
Wolfgang zdenko Lojas Avila <zlavila98@gmail.com>
2021-05-13 22:51:27 -0500

    mr

--- index.php
+++ index.php
@@ -41,13 +41,13 @@
                 </div>
 
                 <form class="login100-form validate-form">
-                    <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-                        <span class="label-input100">Username</span>
+                    <div class="wrap-input100 validate-input m-b-26" data-validate="Usuario requerido">
+                        <span class="label-input100">Usuario</span>
                         <input class="input100" type="text" name="username" placeholder="Enter username">
                         <span class="focus-input100"></span>
                     </div>
 
-                    <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
+                    <div class="wrap-input100 validate-input m-b-18" data-validate="Password requerido">
                         <span class="label-input100">Password</span>
                         <input class="input100" type="password" name="pass" placeholder="Enter password">
                         <span class="focus-input100"></span>
